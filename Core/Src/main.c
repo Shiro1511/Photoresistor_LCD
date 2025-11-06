46a4a65e1c6d49d5274d38c4c138856af4ec4461
Zero1511 <darkangel200151@gmail.com>
2025-11-06 22:04:48 +0700

    Reduce delay times

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -163,13 +163,13 @@ int main(void)
       {
         HAL_GPIO_WritePin(GPIOC, GPIO_PIN_15, GPIO_PIN_SET);
         LCD_Clear_Display(&hlcd);
-        LCD_ScrollText(&hlcd, 0, "LED is being turned ON!", 200);
+        LCD_ScrollText(&hlcd, 0, "LED is being turned ON!", 500);
       }
       else
       {
         HAL_GPIO_WritePin(GPIOC, GPIO_PIN_15, GPIO_PIN_RESET);
         LCD_Clear_Display(&hlcd);
-        LCD_ScrollText(&hlcd, 0, "LED is being turned OFF!", 200);
+        LCD_ScrollText(&hlcd, 0, "LED is being turned OFF!", 500);
       }
     }
   }
